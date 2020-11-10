4f51982c95f8460f98d0cd1530e54600389e210a
Vincent Chiu <iquping@gmail.com>
2020-11-10 23:18:08 +0800

    修改图表和菜单

--- js/mycharts.js
+++ js/mycharts.js
@@ -4,7 +4,7 @@ var myChart1 = echarts.init(document.getElementById('myChart1'));
 // 指定图表的配置项和数据
 var option1 = {
     title: {
-        text: '堆叠区域图'
+        text: '总业绩走势'
     },
     tooltip: {
         trigger: 'axis',
@@ -16,7 +16,7 @@ var option1 = {
         }
     },
     legend: {
-        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        data: ['时令水果', '新鲜蔬菜', '海鲜水产', '速食冷冻', '肉禽蛋品']
     },
     toolbox: {
         feature: {
@@ -43,35 +43,35 @@ var option1 = {
     ],
     series: [
         {
-            name: '邮件营销',
+            name: '时令水果',
             type: 'line',
             stack: '总量',
             areaStyle: {},
             data: [120, 132, 101, 134, 90, 230, 210]
         },
         {
-            name: '联盟广告',
+            name: '新鲜蔬菜',
             type: 'line',
             stack: '总量',
             areaStyle: {},
             data: [220, 182, 191, 234, 290, 330, 310]
         },
         {
-            name: '视频广告',
+            name: '海鲜水产',
             type: 'line',
             stack: '总量',
             areaStyle: {},
             data: [150, 232, 201, 154, 190, 330, 410]
         },
         {
-            name: '直接访问',
+            name: '速食冷冻',
             type: 'line',
             stack: '总量',
             areaStyle: {},
             data: [320, 332, 301, 334, 390, 330, 320]
         },
         {
-            name: '搜索引擎',
+            name: '肉禽蛋品',
             type: 'line',
             stack: '总量',
             label: {
@@ -92,11 +92,11 @@ var myChart2 = echarts.init(document.getElementById('myChart2'));
 
 var option2 = {
     title: {
-        text: '商品的销售量'
+        text: '主品类销量走势'
     },
     tooltip: {},
     legend: {
-        data: ['品类', 'xx1', 'xx2', 'xx3', 'xx4', 'xx5'],
+        data: ['时令水果', '新鲜蔬菜', '海鲜水产', '速食冷冻', '肉禽蛋品'],
 
     },
     xAxis: {
@@ -104,75 +104,54 @@ var option2 = {
     },
     yAxis: {},
     series: [{
-        name: '品类',
+        name: '时令水果',
         type: 'line',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [5, 20, 36, 10, 2, 20]
     },
     {
-        name: '品类',
+        name: '新鲜蔬菜',
+        type: 'line',
+        data: [10, 40, 36, 20, 25, 30]
+    },
+    {
+        name: '海鲜水产',
+        type: 'line',
+        data: [15, 32, 25, 28, 16, 38]
+    },
+    {
+        name: '速食冷冻',
+        type: 'line',
+        data: [12, 26, 29, 42, 36, 28]
+    },
+    {
+        name: '肉禽蛋品',
+        type: 'line',
+        data: [8, 16, 14, 22, 13, 27]
+    },
+    {
+        name: '时令水果',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [5, 20, 36, 10, 2, 20]
     },
     {
-        name: 'xx1',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx1',
+        name: '新鲜蔬菜',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [10, 40, 36, 20, 25, 30]
     },
     {
-        name: 'xx2',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-
-    {
-        name: 'xx3',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx3',
+        name: '海鲜水产',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [15, 32, 25, 28, 16, 38]
     },
     {
-        name: 'xx4',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx4',
+        name: '速食冷冻',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [12, 26, 29, 42, 36, 28]
     },
     {
-        name: 'xx5',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx5',
+        name: '肉禽蛋品',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [8, 16, 14, 22, 13, 27]
     },
     ]
 };
@@ -181,87 +160,65 @@ var myChart3 = echarts.init(document.getElementById('myChart3'));
 
 var option3 = {
     title: {
-        text: '商品的销售额'
+        text: '副品类销量走势'
     },
     tooltip: {},
     legend: {
-        data: ['品类', 'xx1', 'xx2', 'xx3', 'xx4', 'xx5'],
-
+        data: ['日用百货', '滋补干货', '美妆个护', '母婴保健', '酒水饮料']
     },
     xAxis: {
         data: ['9月1日', '9月2日', '9月3日', '9月4日', '9月5日']
     },
     yAxis: {},
     series: [{
-        name: '品类',
+        name: '日用百货',
         type: 'line',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [5, 20, 36, 10, 2, 20]
     },
     {
-        name: '品类',
+        name: '滋补干货',
+        type: 'line',
+        data: [10, 40, 36, 20, 25, 30]
+    },
+    {
+        name: '美妆个护',
+        type: 'line',
+        data: [15, 32, 25, 28, 16, 38]
+    },
+    {
+        name: '母婴保健',
+        type: 'line',
+        data: [12, 26, 29, 42, 36, 28]
+    },
+    {
+        name: '酒水饮料',
+        type: 'line',
+        data: [8, 16, 14, 22, 13, 27]
+    },
+    {
+        name: '日用百货',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [5, 20, 36, 10, 2, 20]
     },
     {
-        name: 'xx1',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx1',
+        name: '滋补干货',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [10, 40, 36, 20, 25, 30]
     },
     {
-        name: 'xx2',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-
-    {
-        name: 'xx3',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx3',
+        name: '美妆个护',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [15, 32, 25, 28, 16, 38]
     },
     {
-        name: 'xx4',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx4',
+        name: '母婴保健',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [12, 26, 29, 42, 36, 28]
     },
     {
-        name: 'xx5',
-        type: 'line',
-        data: [10, 40, 36, 20, 20, 30],
-
-
-
-    },
-    {
-        name: 'xx5',
+        name: '酒水饮料',
         type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        data: [8, 16, 14, 22, 13, 27]
     },
     ]
 };
@@ -271,7 +228,7 @@ var option4 = {
     backgroundColor: '#2c343c',
 
     title: {
-        text: 'Customized Pie',
+        text: '主品类业绩占成',
         left: 'center',
         top: 20,
         textStyle: {
@@ -294,16 +251,16 @@ var option4 = {
     },
     series: [
         {
-            name: '访问来源',
+            name: '当前品类占总销售额的比例',
             type: 'pie',
             radius: '55%',
             center: ['50%', '50%'],
             data: [
-                {value: 335, name: '直接访问'},
-                {value: 310, name: '邮件营销'},
-                {value: 274, name: '联盟广告'},
-                {value: 235, name: '视频广告'},
-                {value: 400, name: '搜索引擎'}
+                { value: 335, name: '时令水果' },
+                { value: 310, name: '新鲜蔬菜' },
+                { value: 274, name: '海鲜水产' },
+                { value: 235, name: '速食冷冻' },
+                { value: 400, name: '肉禽蛋品' }
             ].sort(function (a, b) { return a.value - b.value; }),
             roseType: 'radius',
             label: {
@@ -337,7 +294,7 @@ var option5 = {
     backgroundColor: '#2c343c',
 
     title: {
-        text: 'Customized Pie',
+        text: '副品类业绩占成',
         left: 'center',
         top: 20,
         textStyle: {
@@ -360,16 +317,16 @@ var option5 = {
     },
     series: [
         {
-            name: '访问来源',
+            name: '当前品类占总销售额的比例',
             type: 'pie',
             radius: '55%',
             center: ['50%', '50%'],
             data: [
-                {value: 335, name: '直接访问'},
-                {value: 310, name: '邮件营销'},
-                {value: 274, name: '联盟广告'},
-                {value: 235, name: '视频广告'},
-                {value: 400, name: '搜索引擎'}
+                { value: 335, name: '日用百货' },
+                { value: 310, name: '滋补干货' },
+                { value: 274, name: '联盟广告' },
+                { value: 235, name: '母婴保健' },
+                { value: 400, name: '酒水饮料' }
             ].sort(function (a, b) { return a.value - b.value; }),
             roseType: 'radius',
             label: {
@@ -407,8 +364,8 @@ charts.push(myChart2)
 charts.push(myChart3)
 charts.push(myChart4)
 charts.push(myChart5)
-window.onresize = function(){
-    for(var i = 0; i < charts.length; i++){
+window.onresize = function () {
+    for (var i = 0; i < charts.length; i++) {
         charts[i].resize();
     }
 };
